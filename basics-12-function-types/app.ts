5a21db1afd8fba197739e82e2f0fccb6198238f9
Ronan Parkinson <parkinson95@hotmail.co.uk>
2023-10-16 20:43:23 +0100

    Add files via upload
    //method overloading
    //optional chaining
    //nullish coalescing

--- basics-12-function-types/app.ts
+++ basics-12-function-types/app.ts
@@ -8,6 +8,11 @@ type employee = {
     startDate: Date;
 }
 
+type Owner = {
+    name: string;
+    startDate: Date;
+}
+
 type ElevatedEmployee = Admin & employee;
 
 const e1 = {
@@ -23,14 +28,7 @@ type Numeric = number | boolean;
 
 type Uni = Combine & Numeric;
 
-function add(a: Combine, b: Combine){
-    if(typeof a === 'string' || typeof b === 'string'){
-         return a.toString() + b.toString();
-    }
-    return a + b;
-}
-
-type UnknownEmployee = employee | Admin;
+type UnknownEmployee = employee | Admin | Owner;
 
 function printEmployeeInfo(emp: UnknownEmployee){
     console.log('Name:' + emp.name);
@@ -57,3 +55,63 @@ class Truck{
         console.log('Loading cargo: ' + amount);
     }
 }
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle){
+    vehicle.drive();
+    if(vehicle instanceof Truck)
+    vehicle.loadCargo(11);
+}
+
+//typecasting below
+//DOM use
+const userInputElement = document.getElementById('user-input');
+const para = document.querySelector('p');
+//const userInputOutput = <HTMLInputElement>document.getElementById('message-output');
+//another option to the line above is:
+const userInput = document.getElementById('message-output')! as HTMLInputElement;
+userInput.value = 'hello';
+// if(userInputElement){
+//     (userInputElement as HTMLInputElement).value = 'Hi There'
+// }
+
+//index types
+interface ErrorContainer{ //{email: 'not a vaild email'}
+    [prop: string]: string;
+}
+
+const errorBag: ErrorContainer = {
+    email:  'asds@test.com',
+    userName: 'ronan'
+};
+
+//method overloading
+function adding(a: number, b: number): number;
+function adding(a: string, b: string): string;
+function adding(a: string, b: number): string;
+function adding(a: number, b: string): string;
+function adding(a: Combine, b: Combine) {
+  if (typeof a === 'string' || typeof b === 'string'){
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+const result = adding('Ronan', 'Pa');
+result.split(' ');
+
+//optional chaining
+const fetchedUserData = {
+  id: 'u1',
+  name: 'Max',
+  //job: { title: 'CTO', description: 'My own company' }
+};
+//the ? makes the field that was called optional to gather info from, can be used for varaiables and parameters too 
+console.log(fetchedUserData?.job?.title);
+
+const userInputt = null;
+const storedData = userInputt || 'DE'
--- basics-12-function-types/functions.ts
+++ basics-12-function-types/functions.ts
@@ -6,7 +6,7 @@ function printResult(num: number): void {
   console.log('Result: ' + num);
 }
 
-function addAndHandle(n1: number, n2: number, cb: (number) => void) {
+function addAndHandle(n1: number, n2: number, cb: (arg0: number) => void) {
   const result = n1 + n2;
   cb(result);
 }
@@ -28,13 +28,13 @@ console.log(combineValues(8, 8));
 // let someValue: undefined;
 
 
-function stringOrNumber(n1, n2){
-  if(typeof n1 === 'string' && typeof n2 === 'string'){
-      return +n1 + +n2;
-  } else{
-      return n1 + n2;
-  }
-}
+// function stringOrNumber(n1, n2){
+//   if(typeof n1 === 'string' && typeof n2 === 'string'){
+//       return +n1 + +n2;
+//   } else{
+//       return n1 + n2;
+//   }
+// }
 
 let Number1: number | string;
 let Number2: number | string;
@@ -42,7 +42,7 @@ let Number2: number | string;
 Number1 = 'dsds';
 Number2 = 'sdd';
 
-console.log(stringOrNumber(Number1,Number2));
+// console.log(stringOrNumber(Number1,Number2));
 
  const Person = {
   name: 'ron',
